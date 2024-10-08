b37e597fac86c4496bf93437a605610fe7a98c32
Rajuarshad <154614126+iqraarshad199@users.noreply.github.com>
2024-10-08 21:25:56 +0500

    Delete LinkedList.h

--- LinkedList.h
+++
@@ -1,18 +0,0 @@
-#pragma once
-#include "Node.h"
-#include<iostream>
-class LinkedList
-{
-private:
-	Node*head;
-public:
-
-	LinkedList();
-	bool isempty();	void insertstart(int val);
-	void insertend(int val);
-	void deletebegin();
-	void deleteend();
-	void display();
-	~LinkedList();
-};
-

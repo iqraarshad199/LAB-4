6e12eb8c140e579a0e0a873c0c931bbf585bccc8
Rajuarshad <154614126+iqraarshad199@users.noreply.github.com>
2024-10-08 21:24:40 +0500

    Delete Clist.h

--- Clist.h
+++
@@ -1,17 +0,0 @@
-#pragma once
-
-#include "Node.h"
-
-class Clist
-{
-public:
-	Node* head;
-	Clist();
-	bool isempty();	void insertstart(int val);
-	void insertend(int val);
-	void deletebegin();
-	void deleteend();
-	void display();
-	~Clist();
-};
-
